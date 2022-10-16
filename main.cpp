5acf19f328ee5c9ae499c5e5f3e0b6209539d0a4
selimsyl <selim.syl07@gmail.com>
2022-10-16 10:31:13 +0300

    dosyalar olusturuldu

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <thread>
+#include "queue.h"
+
+int main() {
+    Queue<int> queue{};
+}
---
+++ queue.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <mutex>
+#include <condition_variable>
+
+template<typename Data>
+class Queue {
+    Queue(std::size_t size) {}
+
+};
